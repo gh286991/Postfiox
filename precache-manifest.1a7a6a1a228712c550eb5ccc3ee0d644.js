6624eed51b4e8e36399f5eb0050910ecaaa1b078
TomJhuang <gh286991@gmail.com>
2019-10-07 00:40:11 +0800

    Updates

---
+++ precache-manifest.1a7a6a1a228712c550eb5ccc3ee0d644.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba6d47d827f2484c5d8c6e17af2a8aa",
+    "url": "/Postfix/index.html"
+  },
+  {
+    "revision": "dc3c7bd221f513c81456",
+    "url": "/Postfix/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "702d4382a438d9de2181",
+    "url": "/Postfix/static/js/2.9a693493.chunk.js"
+  },
+  {
+    "revision": "dc3c7bd221f513c81456",
+    "url": "/Postfix/static/js/main.aa547a19.chunk.js"
+  },
+  {
+    "revision": "5aee16c28e7c6bae4dd4",
+    "url": "/Postfix/static/js/runtime-main.54ef6cbb.js"
+  }
+]);
